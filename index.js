8fe5575899f3568f4bb6695067fbec58f5d4ab64
anthonykumasaka@gmail.com Kumasaka <anthonykumasaka@gmail.com>
2018-07-24 03:06:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,17 +6,17 @@ function whisper(string) {
   return string.toLowerCase()
 }
 
-describe('logShout(string)', function() {
-  it('calls console.log() its one argument in all caps', function() {
-    const spy = expect.spyOn(console, 'log').andCallThrough()
+// describe('logShout(string)', function() {
+//   it('calls console.log() its one argument in all caps', function() {
+//     const spy = expect.spyOn(console, 'log').andCallThrough()
 
-    logShout('hello')
+//     logShout('hello')
 
-    expect(spy).toHaveBeenCalledWith('HELLO')
+//     expect(spy).toHaveBeenCalledWith('HELLO')
 
-    console.log.restore()
-  })
-})
+//     console.log.restore()
+//   })
+// })
 
 function logShout(string) {
   console.log(string.toUpperCase())
