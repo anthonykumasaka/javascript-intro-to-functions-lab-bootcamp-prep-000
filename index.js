e3c2da14cb3962c869269429bb445213a1f58d93
anthonykumasaka@gmail.com Kumasaka <anthonykumasaka@gmail.com>
2018-07-24 02:45:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -27,7 +27,5 @@ function sayHiToGrandma(string) {
     return "YES INDEED!"
   } else if (string == "I love you, Grandma.") {
     return "I love you, too."
-  } else {
-    return "YES INDEED!"
   }
 }
