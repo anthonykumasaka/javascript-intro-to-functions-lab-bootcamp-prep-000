40abae4810fef0c0625d12763c6a9db6ec712a5a
anthonykumasaka@gmail.com Kumasaka <anthonykumasaka@gmail.com>
2018-07-24 03:07:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,18 +6,6 @@ function whisper(string) {
   return string.toLowerCase()
 }
 
-// describe('logShout(string)', function() {
-//   it('calls console.log() its one argument in all caps', function() {
-//     const spy = expect.spyOn(console, 'log').andCallThrough()
-
-//     logShout('hello')
-
-//     expect(spy).toHaveBeenCalledWith('HELLO')
-
-//     console.log.restore()
-//   })
-// })
-
 function logShout(string) {
   console.log(string.toUpperCase())
 }
@@ -40,12 +28,12 @@ function logWhisper(string) {
 //   })
 // })
 
-// function sayHiToGrandma(string) {
-//   if (string === string.toLowerCase) {
-//     return "I can't hear you!"
-//   } else if (string === string.toUpperCase) {
-//     return "YES INDEED!"
-//   } else if (string == "I love you, Grandma.") {
-//     return "I love you, too."
-//   }
-// }
+function sayHiToGrandma(string) {
+  if (string === string.toLowerCase) {
+    return "I can't hear you!"
+  } else if (string === string.toUpperCase) {
+    return "YES INDEED!"
+  } else if (string == "I love you, Grandma.") {
+    return "I love you, too."
+  }
+}
